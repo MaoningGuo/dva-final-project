c19195848c547e5ca5a706caf4eaa9c943d038a2
Sophia <alian20062006@hotmail.com>
2022-04-15 18:50:10 -0400

    hellow world, initing gh pages

---
+++ build/precache-manifest.200eb7ae00d25cef6de9ffabb1fe02ca.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "cd9bc5c8d352f983fb69",
+    "url": "/dva-final-project/static/css/main.cde9ec6a.chunk.css"
+  },
+  {
+    "revision": "cd9bc5c8d352f983fb69",
+    "url": "/dva-final-project/static/js/main.525da30a.chunk.js"
+  },
+  {
+    "revision": "5a45fd60f201aabd1280",
+    "url": "/dva-final-project/static/js/runtime~main.a4b02962.js"
+  },
+  {
+    "revision": "6ccaca18b0e8792403a8",
+    "url": "/dva-final-project/static/js/2.81c6594e.chunk.js"
+  },
+  {
+    "revision": "a03a2b79c7dfa4b66d3d",
+    "url": "/dva-final-project/static/js/3.7fa365a8.chunk.js"
+  },
+  {
+    "revision": "06e733283fa43d1dd57738cfc409adbd",
+    "url": "/dva-final-project/static/media/logo.06e73328.svg"
+  },
+  {
+    "revision": "1cb4066564e0f2a8739677a55e37ad7a",
+    "url": "/dva-final-project/index.html"
+  }
+];
