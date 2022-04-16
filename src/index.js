66ca391076d9ca2f2f2483b0dd5506d42f4e9d61
Sophia <alian20062006@hotmail.com>
2022-04-16 00:49:17 -0400

    fixing react router, adding base name

--- src/index.js
+++ src/index.js
@@ -11,7 +11,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <Router>
+    <Router basename='dva-final-project'>
       <Routes className="root-router">
         <Route path='/' element={<LandingComponent />} />
         <Route exact path='/bars' element={<BarCharts />} />
