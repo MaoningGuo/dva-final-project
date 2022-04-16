b42ccc49cd912f7ab8cb39c98037be5291c4ae5a
Sophia <alian20062006@hotmail.com>
2022-04-15 21:02:37 -0400

    some working bar chart

---
+++ src/BarChart.js
@@ -0,0 +1,133 @@
+import React, { useRef, useEffect, createRef } from "react";
+import * as d3 from "d3"
+import rd3 from 'react-d3-library';
+
+import alphabets from "./alphabet.csv"
+
+const RD3Component = rd3.Component;
+
+function createBarChart(data, {
+    x = (d, i) => i, // given d in data, returns the (ordinal) x-value
+    y = d => d, // given d in data, returns the (quantitative) y-value
+    title, // given d in data, returns the title text
+    marginTop = 20, // the top margin, in pixels
+    marginRight = 0, // the right margin, in pixels
+    marginBottom = 30, // the bottom margin, in pixels
+    marginLeft = 40, // the left margin, in pixels
+    width = 640, // the outer width of the chart, in pixels
+    height = 400, // the outer height of the chart, in pixels
+    xDomain, // an array of (ordinal) x-values
+    xRange = [marginLeft, width - marginRight], // [left, right]
+    yType = d3.scaleLinear, // y-scale type
+    yDomain, // [ymin, ymax]
+    yRange = [height - marginBottom, marginTop], // [bottom, top]
+    xPadding = 0.1, // amount of x-range to reserve to separate bars
+    yFormat, // a format specifier string for the y-axis
+    yLabel, // a label for the y-axis
+    color = "currentColor", // bar fill color
+    nodeRef
+} = {}) {
+    // Compute values.
+    const X = d3.map(data, x);
+    const Y = d3.map(data, y);
+
+    // Compute default domains, and unique the x-domain.
+    if (xDomain === undefined) xDomain = X;
+    if (yDomain === undefined) yDomain = [0, d3.max(Y)];
+    console.log("xDomain")
+    console.log(xDomain)
+    xDomain = new d3.InternSet(xDomain);
+
+    console.log(X);
+    console.log(Y);
+    console.log(yRange);
+    console.log(yDomain);
+    // Omit any data not present in the x-domain.
+    const I = d3.range(X.length).filter(i => xDomain.has(X[i]));
+
+    // Construct scales, axes, and formats.
+    const xScale = d3.scaleBand(xDomain, xRange).padding(xPadding);
+    const yScale = yType(yDomain, yRange);
+    const xAxis = d3.axisBottom(xScale).tickSizeOuter(0);
+    const yAxis = d3.axisLeft(yScale).ticks(height / 40, yFormat);
+
+    // Compute titles.
+    if (title === undefined) {
+        const formatValue = yScale.tickFormat(100, yFormat);
+        title = i => `${X[i]}\n${formatValue(Y[i])}`;
+    } else {
+        const O = d3.map(data, d => d);
+        const T = title;
+        title = i => T(O[i], i, data);
+    }
+
+    var svg = d3.select(nodeRef.current);
+    svg.selectAll("*").remove(); // Clear svg content before adding new elements  
+
+    // svg.append("svg")
+    //     .attr("width", width)
+    //     .attr("height", height)
+    //     .attr("viewBox", [0, 0, width, height])
+    //     .attr("style", "max-width: 100%; height: auto; height: intrinsic;");
+
+    svg.append("g")
+        .attr("transform", `translate(${marginLeft},0)`)
+        .call(yAxis)
+        .call(g => g.select(".domain").remove())
+        .call(g => g.selectAll(".tick line").clone()
+            .attr("x2", width - marginLeft - marginRight)
+            .attr("stroke-opacity", 0.1))
+        .call(g => g.append("text")
+            .attr("x", -marginLeft)
+            .attr("y", 10)
+            .attr("fill", "currentColor")
+            .attr("text-anchor", "start")
+            .text(yLabel));
+
+    const bar = svg.append("g")
+        .attr("fill", color)
+        .selectAll("rect")
+        .data(I)
+        .join("rect")
+        .attr("x", i => xScale(X[i]))
+        .attr("y", i => yScale(Y[i]))
+        .attr("height", i => yScale(0) - yScale(Y[i]))
+        .attr("width", xScale.bandwidth());
+
+    if (title) bar.append("title")
+        .text(title);
+
+    svg.append("g")
+        .attr("transform", `translate(0,${height - marginBottom})`)
+        .call(xAxis);
+
+    return svg.node;
+}
+
+const BarChart = () => {
+    const svgRef = React.useRef(null);
+    const properties = {
+        x: d => d.letter,
+        y: d => d.frequency,
+        yFormat: "%",
+        yLabel: "↑ Frequency",
+        height: 500,
+        color: "steelblue",
+        nodeRef: svgRef
+    }
+
+    d3.csv(alphabets).then((data) => {
+        // console.log(data)
+        createBarChart(data, properties);
+    })
+
+    return (
+            <svg ref={svgRef} width="800" height="600"/>
+    );
+
+    // return <div>
+    //     {this.state.node}
+    // </div>
+}
+
+export default BarChart;
